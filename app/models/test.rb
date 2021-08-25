a6d7ace41a08d8ef1d63c602e763ee5a48d7b60c
ZayatsAliaksei <legoza@narod.ru>
2021-08-25 10:22:00 +0300

    add classname&nicknames

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,7 +2,7 @@ class Test < ApplicationRecord
   has_and_belongs_to_many :users, join_table: 'users_tests'
   has_many :questions
   belongs_to :category
-  belongs_to :user
+  belongs_to :author,foreign_key: "user_id", class_name: 'User'
 
   def sort_tests_by_category(category)
     Test.joins('INNER JOIN categories ON categories.id = tests.category_id')
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_and_belongs_to_many :tests, join_table: 'users_tests'
-  has_many :tests
+  has_many :quiz, class_name: 'Test'
 
   def user_tests(test_level)
     self.tests.where(level: test_level)
