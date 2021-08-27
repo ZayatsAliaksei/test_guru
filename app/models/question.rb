4bf21c2c566c9e1e3540527f8eb312ba9fa0a527
ZayatsAliaksei <legoza@narod.ru>
2021-08-28 01:38:18 +0300

    fix

--- app/models/question.rb
+++ app/models/question.rb
@@ -6,6 +6,7 @@ class Question < ApplicationRecord
   # validates :answers, length: { minimum: 1, maximum: 4 } -- такой вариант может быть использован??
   validate :number_of_answers
 
+  private
   def number_of_answers
     errors.add(:answers, "there should be from 1 to 4 answers") if answers.size > 4 || answers.size < 1
   end
