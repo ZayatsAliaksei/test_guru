37cda0ed8e46d3dd283e7da5fc5393edb1a02084
ZayatsAliaksei <legoza@narod.ru>
2021-08-28 01:34:31 +0300

    add scope methods and validate models

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,13 @@
 class Question < ApplicationRecord
   belongs_to :test
   has_many :answers
+
+  validate :body, presence: true
+  # validates :answers, length: { minimum: 1, maximum: 4 } -- такой вариант может быть использован??
+  validate :number_of_answers
+
+  def number_of_answers
+    errors.add(:answers, "there should be from 1 to 4 answers") if answers.size > 4 || answers.size < 1
+  end
+
 end
--- app/models/test.rb
+++ app/models/test.rb
@@ -1,12 +1,18 @@
 class Test < ApplicationRecord
+
   has_and_belongs_to_many :users, join_table: 'users_tests'
   has_many :questions
   belongs_to :category
-  belongs_to :author,foreign_key: "user_id", class_name: 'User'
+  belongs_to :author, foreign_key: "user_id", class_name: 'User'
 
-  def sort_tests_by_category(category)
-    Test.joins('INNER JOIN categories ON categories.id = tests.category_id')
-        .where("categories.title = #{category}").order(title: :desc)
-        .pluck(:title)
-  end
+
+  scope :sort_tests_by_category, -> (category) { joins(:category).where(categories: { title: category })}
+  scope :simple_tests, -> { where(level: 0..1) }
+  scope :regular_tests, -> { where(level: 2..4) }
+  scope :hard_level, -> { where(level: 5..Float::INFINITY) }
+
+  validate :title, presence: true, uniqueness: true
+  validate :level, uniqueness: true
+  validate :level, numericality: { only_integer: true, greater_than: 0}
+
 end
