78ad4f30fe6226bcf6cabbfd7dd25c9198106fbc
ZayatsAliaksei <legoza@narod.ru>
2021-08-29 22:01:03 +0300

    fix test-methods

--- app/models/test.rb
+++ app/models/test.rb
@@ -5,14 +5,17 @@ class Test < ApplicationRecord
   belongs_to :category
   belongs_to :author, foreign_key: "user_id", class_name: 'User'
 
-
-  scope :sort_tests_by_category, -> (category) { joins(:category)
+  scope :sort_tests, -> (category) { joins('INNER JOIN categories ON categories.id = tests.category_id')
                                                    .where(categories: { title: category })
                                                    .order(title: :desc) }
 
   scope :simple_tests, -> { where(level: 0..1) }
   scope :regular_tests, -> { where(level: 2..4) }
   scope :hard_tests, -> { where(level: 5..Float::INFINITY) }
+
+  def self.sort_tests_by_category(category)
+    sort_tests(category).pluck(:title)
+  end
 
   validates :title,uniqueness: { scope: :level, message: "Test with the same title and lvl was create early" }
   validates :level, numericality: { only_integer: true, greater_than: 0}
