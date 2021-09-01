8cf292686ffb796eef0b2a3a58de33db02ce89cf
ZayatsAliaksei <legoza@narod.ru>
2021-09-02 01:45:12 +0300

    add routes and controllers

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,44 @@
 class QuestionsController < ApplicationController
 
-  def index
+  before_action :find_test, only: %i[index]
+  before_action :find_question, only: %i[show destroy create]
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
+  #http://127.0.0.1:3000/tests/1/questions
+  def index
+     render inline: "<h1><b>Test:</b> <%= @test.title %></h1><p><%= @test.questions.each do |q| %> <p> <%= q.body %></p><% end %></p>"
+  end
+  #http://127.0.0.1:3000/questions/1
+  def show
+    # render inline: "<h1><b>Question:</b></h1><%= @question.body %>"
+  end
+
+  #  http://127.0.0.1:3000/tests/1/questions/new
+  def create
+    Question.create({:body=>params[:question][:body],:test_id=> params[:test_id]})
+  end
+
+  def destroy
+    @question.destroy!
+    redirect_to test_questions_path(@question.test_id)
+  end
+
+  def get_test_questions
+    @test.find
+  end
+
+  private
+
+  def find_test
+    @test = Test.find(params[:test_id])
+  end
+
+  def find_question
+    @question = Question.find(params[:id])
+  end
+
+  def rescue_with_question_not_found
+    render plain: 'Question was not found'
   end
 
 end
--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,11 +1,22 @@
 class TestsController < ApplicationController
-
+  before_action :find_test
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_test_not_found
   def index
-    @tests = Test.all
+    @tests = Test.all.map
   end
 
-  def  test_questions_list(test)
+  def show
+    render inline:  '<%= @test.title %>'
+  end
 
+  private
+
+  def find_test
+    @test = Test.find(params[:id])
+  end
+
+  def rescue_with_test_not_found
+    render plain: 'Test was not found'
   end
 
 end
